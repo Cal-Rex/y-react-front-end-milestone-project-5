e2639ebe33e65f1cc0f64e1c6e105059d1cfe08b
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-10 10:51:02 +0000

    amend active pseudoclass ovverides on button styles

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,12 @@
 import { useState, useRef } from "react";
 import styles from "../styles/Avatar.module.css";
 import { Button, Overlay } from "react-bootstrap";
-import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext'
+import { useSetCurrentUser } from '../contexts/CurrentUserContext'
 import axios from "axios";
 
 
 const Avatar = (props) => {
-    const {src, height = 45, message} = props;
+    const { src, height = 45, message } = props;
     const [show, setShow] = useState(false);
     const target = useRef(null);
     const setCurrentUser = useSetCurrentUser();
@@ -20,37 +20,42 @@ const Avatar = (props) => {
         }
     };
 
-return (
-    <div>
-        <span className={styles.Username}>{message}</span>
-        <img
-            className={styles.ProfilePic}
-            src={src}
-            height={height}
-            width={height}
-            alt="Profile Avatar"
-            ref={target}
-            onClick={() => setShow(!show)}
-        />
-         <Overlay target={target.current} show={show} placement="bottom">
-        {({ placement, arrowProps, show: _show, popper, ...props }) => (
-          <div
-            {...props}
-            style={{
-              backgroundColor: '#00000000',
-              padding: '2px 10px',
-              color: '#fafafa',
-              borderRadius: 3,
-              ...props.style,
-            }}
-          >
-            <Button className={styles.AMButton} onClick={handleLogOut}>Log Out</Button>
-          </div>
-        )}
-      </Overlay>
-            
-    </div>
-)
+    return (
+        <div>
+            <span className={styles.Username}>{message}</span>
+            <img
+                className={styles.ProfilePic}
+                src={src}
+                height={height}
+                width={height}
+                alt="Profile Avatar"
+                ref={target}
+                onClick={() => setShow(!show)}
+            />
+            <Overlay target={target.current} show={show} placement="bottom-end">
+                {({ placement, arrowProps, show: _show, popper, ...props }) => (
+                    <div
+                        {...props}
+                        style={{
+                            backgroundColor: '#00000000',
+                            padding: '2px 10px',
+                            color: '#fafafa',
+                            borderRadius: 3,
+                            ...props.style,
+                        }}
+                    >
+                        <Button
+                            className={styles.AMButton}
+                            onClick={handleLogOut}
+                        >
+                            Log Out
+                        </Button>
+                    </div>
+                )}
+            </Overlay>
+
+        </div>
+    )
 }
 
 export default Avatar;
