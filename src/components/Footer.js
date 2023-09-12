8af796af3d24b347be1611606b9bff185e59b2bd
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-12 08:58:17 +0000

    add footer nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Button, Col, Container, Form, FormControl, Nav, Navbar, NavLink, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
     return (
-        <Navbar className={styles.Footer} fixed="bottom">
-            <Nav className="m-auto">
-                <NavLink to="/"> +</NavLink>
-                {/* <Nav.Link href="#link">Link</Nav.Link> */}
-            </Nav>
-        </Navbar>
+        <Container fluid className={`${styles.Footer} fixed-bottom`}>
+                <Row>
+                    <Col className='text-right' xs={{ span: 4 }} md={{ span: 4 }}>
+                        <Link to="/posts/create">
+                            <i class="fa-solid fa-fire-flame-curved fa-2xl"></i>
+                        </Link>
+                    </Col>
+                    <Col className='text-center' xs={{ span: 4 }} md={{ span: 4 }}>
+                        <Link to="/posts/create">
+                            <i class="fa-regular fa-square-plus fa-2xl"></i>
+                        </Link>
+                    </Col>
+                    <Col className='text-left' xs={{ span: 4 }} md={{ span: 4 }}>
+                        <Link>
+                            <i class="fa-solid fa-user-group fa-2xl"></i>
+                        </Link>
+                    </Col>
+                </Row>
+
+        </Container>
     )
 }
 
